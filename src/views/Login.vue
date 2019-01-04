651a9a44cafa334fdc129b2a1ac8e75d8801f1d3
PaulienBuskens <32159110+PaulienBuskens@users.noreply.github.com>
2019-01-04 16:26:31 +0100

    login_page

---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Login">
+    <img class="logo" src="../assets/arcon_logo.png" alt="arcon logo">
+    <img class="profile_pic" src="../assets/boy.jpg" alt="boy">
+    <img src="../assets/button_login.png" alt="button">
+    <p>Sign up</p>
+  </div>
+</template>
+
+<style lang="scss">
+
+  .logo{
+    width: 90%;
+    margin-bottom: 8%;
+  }
+
+  .profile_pic {
+    border-radius: 100%;
+    margin-bottom: 12%;
+  }
+
+  p{
+    text-decoration: underline;
+  }
+
+</style>
